845cdf3a724d5bde05ad4aa21b5160dfbe129bc7
jair oliveira <jroliveira.sa@gmail.com>
2025-04-22 17:13:28 -0300

    adjustments in slack class

--- slack/request.rb
+++ slack/request.rb
@@ -1,17 +1,31 @@
+# slack/request.rb
+require 'httparty'
+require 'json'
+require 'dotenv/load'
+
 module Slack
   class Request
+    SLACK_URL = ENV['SLACK_URL'] || 'https://slack.com/api/'
     SLACK_CHANNEL_ID = ENV['SLACK_CHANNEL_ID']
     SLACK_BOT_TOKEN = ENV['SLACK_BOT_TOKEN']
-    SLACK_URL = ENV['SLACK_URL']
 
     def self.call
-      response = HTTParty.post("#{SLACK_URL}conversations.history?channel=#{SLACK_CHANNEL_ID}", headers: headers)
-      JSON.parse(response.body)['message'] || []
+      response = HTTParty.get(
+        "#{SLACK_URL}conversations.history?channel=#{SLACK_CHANNEL_ID}",
+        headers: headers
+      )
+      puts "Response: #{response.body}"
+      JSON.parse(response.body)['messages'] || []
+    rescue => e
+      puts "Slack API error: #{e.message}"
+      []
     end
+
+    private
 
     def self.headers
       {
-        'Authorization' => "Bearer #{SLACK_TOKEN}",
+        'Authorization' => "Bearer #{SLACK_BOT_TOKEN}",
         'Content-Type' => 'application/json'
       }
     end
